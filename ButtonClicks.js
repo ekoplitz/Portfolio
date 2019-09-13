e4281f46b9e8ece956dcbad518e1be112cdeb657
ekoplitz <emk1221@gmail.com>
2019-09-13 01:33:54 -0400

    Add files via upload

---
+++ ButtonClicks.js
@@ -0,0 +1,24 @@
+﻿"use strict"
+
+var selectedButton = null;
+
+function onButtonPressed(buttonId) {
+
+    var pressedButton = document.getElementById(buttonId);
+
+    //  Do nothing if pressed button is already selected
+    if (selectedButton == pressedButton) return;
+
+    // Set pressed button to selected button
+    if (selectedButton != null) {
+        selectedButton.style = "button.textbutton";
+    }
+    
+    if (pressedButton != null) {
+        pressedButton.style.color = "black";
+        pressedButton.style.transform = "translate(10%, 0%)";
+
+    }
+
+    selectedButton = pressedButton;
+}
